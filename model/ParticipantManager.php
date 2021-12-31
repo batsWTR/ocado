3a85cb320100420b80f81873af79c615348dc365
batsWTR <baptiste.wentzler@wanadoo.fr>
2021-12-31 10:37:27 +0100

    ajout fichier participantManager

---
+++ model/ParticipantManager.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('./model/Manager.php');
+
+class ParticipantManager extends Manager{
+    public function addParticipation($owner, $gift, $price){
+        $db = $this->dbconnect();
+        $receve = $db->prepare("INSERT INTO `participant`(`owner_id`, `gift_id`, `price`) VALUES (:owner,:gift,:price)");
+        $receve->execute([
+            'owner' => $owner,
+            'gift' => $gift,
+            'price' => $price
+        ]);
+        $receve->fetchAll();
+
+        $db = null;
+        $receve = null;
+    }
+
+    public function deleteParticipation($id){
+        $db = $this->dbconnect();
+        $receve = $db->prepare("DELETE FROM `participant` WHERE `id`=:id");
+        $receve->execute([
+            'id' => $id
+        ]);
+        $receve->fetchAll();
+    }
+
+    public function getParticipant($gift_id){
+
+    }
+}
